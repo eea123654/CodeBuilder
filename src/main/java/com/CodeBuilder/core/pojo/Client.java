13e01058366f0eb3bf599b7871e25a2304b3422b
eea123654 <784827222@qq.com>
2019-03-07 17:22:40 +0800

    emmm

---
+++ src/main/java/com/CodeBuilder/core/pojo/Client.java
@@ -0,0 +1,31 @@
+package com.CodeBuilder.core.pojo;
+
+public class Client {
+    private int id;
+    private String no;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/CodeBuilder/core/web/HomeController.java
@@ -0,0 +1,28 @@
+package com.CodeBuilder.core.web;
+
+import com.CodeBuilder.core.mapper.ClientMapper;
+import com.CodeBuilder.core.mapper.TableMapper;
+import com.CodeBuilder.core.pojo.Client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    ClientMapper clientMapper;
+
+    @RequestMapping("/home")
+    public String home(Model m) {
+        List<Client> cs=clientMapper.findAll();
+        m.addAttribute("cs",cs);
+        //m.addAttribute("name", "thymeleaf");
+        return "home";
+    }
+
+}
