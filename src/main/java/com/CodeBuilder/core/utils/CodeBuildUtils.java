adb6ff2ccbead2d80af3a4cebcade97c7c643b22
eea123654 <784827222@qq.com>
2019-03-11 17:26:02 +0800

    emmm

---
+++ src/main/java/com/CodeBuilder/core/utils/CodeBuildUtils.java
@@ -0,0 +1,199 @@
+package com.CodeBuilder.core.utils;
+
+import com.CodeBuilder.core.pojo.DataColumn;
+
+import freemarker.template.Template;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 描述：代码生成器
+ * Created by Ay on 2017/5/1.
+ */
+public class CodeBuildUtils {
+
+    private final String AUTHOR = "Ay";
+    private final String CURRENT_DATE = "2017/05/03";
+    private final String tableName = "tm_project_quality_problem";
+    private final String packageName = "com.evada.pm.process.manage";
+    private final String tableAnnotation = "质量问题";
+    private final String URL = "jdbc:postgresql://192.168.3.160:10655/cibpm";
+    private final String USER = "postgres";
+    private final String PASSWORD = "888888";
+    private final String DRIVER = "org.postgresql.Driver";
+    private final String diskPath = "D://";
+    private final String changeTableName = replaceUnderLineAndUpperCase(tableName);
+
+    public Connection getConnection() throws Exception{
+        Class.forName(DRIVER);
+        Connection connection= DriverManager.getConnection(URL, USER, PASSWORD);
+        return connection;
+    }
+
+    public static void main(String[] args) throws Exception{
+        CodeBuildUtils codeBuildUtils = new CodeBuildUtils();
+        codeBuildUtils.generate();
+    }
+
+    public void generate() throws Exception{
+        try {
+            Connection connection = getConnection();
+            DatabaseMetaData databaseMetaData = connection.getMetaData();
+            ResultSet resultSet = databaseMetaData.getColumns(null,"%", tableName,"%");
+            //生成Mapper文件
+            generateMapperFile(resultSet);
+            //生成Dao文件
+            generateDaoFile(resultSet);
+            //生成Repository文件
+            generateRepositoryFile(resultSet);
+            //生成服务层接口文件
+            generateServiceInterfaceFile(resultSet);
+            //生成服务实现层文件
+            generateServiceImplFile(resultSet);
+            //生成Controller层文件
+            generateControllerFile(resultSet);
+            //生成DTO文件
+            generateDTOFile(resultSet);
+            //生成Model文件
+            generateModelFile(resultSet);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }finally{
+
+        }
+    }
+
+    private void generateModelFile(ResultSet resultSet) throws Exception{
+
+        final String suffix = ".java";
+        final String path = diskPath + changeTableName + suffix;
+        final String templateName = "Model.ftl";
+        File mapperFile = new File(path);
+        List<DataColumn> columnClassList = new ArrayList<>();
+        DataColumn columnClass = null;
+        while(resultSet.next()){
+            //id字段略过
+            if(resultSet.getString("COLUMN_NAME").equals("id")) continue;
+            columnClass = new DataColumn();
+            //获取字段名称
+            columnClass.setColumnName(resultSet.getString("COLUMN_NAME"));
+            //获取字段类型
+            columnClass.setColumnType(resultSet.getString("TYPE_NAME"));
+            //转换字段名称，如 sys_name 变成 SysName
+            columnClass.setChangeColumnName(replaceUnderLineAndUpperCase(resultSet.getString("COLUMN_NAME")));
+            //字段在数据库的注释
+            columnClass.setColumnComment(resultSet.getString("REMARKS"));
+            columnClassList.add(columnClass);
+        }
+        Map<String,Object> dataMap = new HashMap<>();
+        dataMap.put("model_column",columnClassList);
+        generateFileByTemplate(templateName,mapperFile,dataMap);
+
+    }
+
+    private void generateDTOFile(ResultSet resultSet) throws Exception{
+        final String suffix = "DTO.java";
+        final String path = "D://" + changeTableName + suffix;
+        final String templateName = "DTO.ftl";
+        File mapperFile = new File(path);
+        Map<String,Object> dataMap = new HashMap<>();
+        generateFileByTemplate(templateName,mapperFile,dataMap);
+    }
+
+    private void generateControllerFile(ResultSet resultSet) throws Exception{
+        final String suffix = "Controller.java";
+        final String path = diskPath + changeTableName + suffix;
+        final String templateName = "Controller.ftl";
+        File mapperFile = new File(path);
+        Map<String,Object> dataMap = new HashMap<>();
+        generateFileByTemplate(templateName,mapperFile,dataMap);
+    }
+
+    private void generateServiceImplFile(ResultSet resultSet) throws Exception{
+        final String suffix = "ServiceImpl.java";
+        final String path = diskPath + changeTableName + suffix;
+        final String templateName = "ServiceImpl.ftl";
+        File mapperFile = new File(path);
+        Map<String,Object> dataMap = new HashMap<>();
+        generateFileByTemplate(templateName,mapperFile,dataMap);
+    }
+
+    private void generateServiceInterfaceFile(ResultSet resultSet) throws Exception{
+        final String prefix = "I";
+        final String suffix = "Service.java";
+        final String path = diskPath + prefix + changeTableName + suffix;
+        final String templateName = "ServiceInterface.ftl";
+        File mapperFile = new File(path);
+        Map<String,Object> dataMap = new HashMap<>();
+        generateFileByTemplate(templateName,mapperFile,dataMap);
+    }
+
+    private void generateRepositoryFile(ResultSet resultSet) throws Exception{
+        final String suffix = "Repository.java";
+        final String path = diskPath + changeTableName + suffix;
+        final String templateName = "Repository.ftl";
+        File mapperFile = new File(path);
+        Map<String,Object> dataMap = new HashMap<>();
+        generateFileByTemplate(templateName,mapperFile,dataMap);
+    }
+
+    private void generateDaoFile(ResultSet resultSet) throws Exception{
+        final String suffix = "DAO.java";
+        final String path = diskPath + changeTableName + suffix;
+        final String templateName = "DAO.ftl";
+        File mapperFile = new File(path);
+        Map<String,Object> dataMap = new HashMap<>();
+        generateFileByTemplate(templateName,mapperFile,dataMap);
+
+    }
+
+    private void generateMapperFile(ResultSet resultSet) throws Exception{
+        final String suffix = "Mapper.xml";
+        final String path = diskPath + changeTableName + suffix;
+        final String templateName = "Mapper.ftl";
+        File mapperFile = new File(path);
+        Map<String,Object> dataMap = new HashMap<>();
+        generateFileByTemplate(templateName,mapperFile,dataMap);
+
+    }
+
+    private void generateFileByTemplate(final String templateName,File file,Map<String,Object> dataMap) throws Exception{
+        Template template = FreeMarkerTemplateUtils.getTemplate(templateName);
+        FileOutputStream fos = new FileOutputStream(file);
+        dataMap.put("table_name_small",tableName);
+        dataMap.put("table_name",changeTableName);
+        dataMap.put("author",AUTHOR);
+        dataMap.put("date",CURRENT_DATE);
+        dataMap.put("package_name",packageName);
+        dataMap.put("table_annotation",tableAnnotation);
+        Writer out = new BufferedWriter(new OutputStreamWriter(fos, "utf-8"),10240);
+        template.process(dataMap,out);
+    }
+
+    public String replaceUnderLineAndUpperCase(String str){
+        StringBuffer sb = new StringBuffer();
+        sb.append(str);
+        int count = sb.indexOf("_");
+        while(count!=0){
+            int num = sb.indexOf("_",count);
+            count = num + 1;
+            if(num != -1){
+                char ss = sb.charAt(count);
+                char ia = (char) (ss - 32);
+                sb.replace(count , count + 1,ia + "");
+            }
+        }
+        String result = sb.toString().replaceAll("_","");
+        return StringUtils.capitalize(result);
+    }
+
+}
--- src/main/java/com/CodeBuilder/core/web/TableController.java
+++ src/main/java/com/CodeBuilder/core/web/TableController.java
@@ -1,24 +1,40 @@
 package com.CodeBuilder.core.web;
 import com.CodeBuilder.core.pojo.Table;
 import com.CodeBuilder.core.service.TableService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class TableController {
 
     @Autowired
     TableService tableService;
 
-    @RequestMapping("/tableList")
-    public String tableList(Model m) {
-        List<Table> tableList=tableService.findList();
-        m.addAttribute("tableList",tableList);
-        return "tableList";
+    /*restful 部分*/
+    @GetMapping("/tables")
+    public PageInfo<Table> list(@RequestParam(value = "start", defaultValue = "1") int start, @RequestParam(value = "size", defaultValue = "10") int size) throws Exception {
+        //PageHelper.startPage(start,size,"id desc");
+        PageHelper.startPage(start,size);
+        List<Table> hs=tableService.findList();
+        System.out.println(hs.size());
+
+        PageInfo<Table> page = new PageInfo<>(hs,5); //5表示导航分页最多有5个，像 [1,2,3,4,5] 这样
+        return page;
+    }
+
+
+    /*页面显示部分*/
+    @RequestMapping(value="/tableList", method=RequestMethod.GET)
+    public ModelAndView tableList() {
+        ModelAndView mv = new ModelAndView("tableList");
+        return mv;
     }
 
 }
